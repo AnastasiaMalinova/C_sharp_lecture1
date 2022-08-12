c9a8cc49728834d3d5892cd73e776f1e3eb19754
Anastasia Malinova <anamali94@mail.ru>
2022-08-12 17:40:06 +0300

    added Example003 Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,7 @@
+﻿// указываем тип данных для целых чисел int
+int numberA = 3;
+int numberB = 5;
+// могли использовать промежуточную переменную
+int result = numberA + numberB;
+Console.WriteLine(result);
+
