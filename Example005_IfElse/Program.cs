db2c903029155f2d3393efa3e7a6dbe7a6c833ed
Anastasia Malinova <anamali94@mail.ru>
2022-08-12 18:50:19 +0300

    Example005 IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,16 @@
+﻿// приглашение пользователя ко вводу, чтобы пользователь понимал, что хотят
+Console.Write("Введите имя пользователя: ");
+
+// тип данных строки, считываем данные при помощи команды Console.ReadLine
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша") // ToLower позволяет все символы строки перевести в нижний регистр
+{
+     Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+ 
