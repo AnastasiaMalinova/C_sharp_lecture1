dd6933719f519ceb4260567d61c29b73c94b0749
Anastasia Malinova <anamali94@mail.ru>
2022-08-12 18:05:50 +0300

    added Example004 Div

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,5 @@
+﻿// указываем контейнер double, позволяющий работать с вещественными числам (нужно при делении, чтобы видеть дробную часть)
+double numberA = 12;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
+
